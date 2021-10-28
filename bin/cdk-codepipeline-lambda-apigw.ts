ca2848a758245a39543935e51e344d5ca1a91bc6
ShengzhenFu <fushengzhen@163.com>
2021-10-28 20:58:30 +0800

    init codepipeline

--- bin/cdk-codepipeline-lambda-apigw.ts
+++ bin/cdk-codepipeline-lambda-apigw.ts
@@ -2,20 +2,14 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { CdkCodepipelineLambdaApigwStack } from '../lib/cdk-codepipeline-lambda-apigw-stack';
+import { MyPipelineStack } from '../lib/codepipeline-stack'
 
 const app = new cdk.App();
-new CdkCodepipelineLambdaApigwStack(app, 'CdkCodepipelineLambdaApigwStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+// new CdkCodepipelineLambdaApigwStack(app, 'CdkCodepipelineLambdaApigwStack', {
+//      env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// });
+new MyPipelineStack(app, 'myPipelineStack', {
+     env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+})
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+app.synth();
