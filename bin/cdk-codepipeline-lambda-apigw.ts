aa29b7958844cce2f4dde794b88eff96b3343d6c
ShengzhenFu <fushengzhen@163.com>
2021-10-29 22:25:06 +0800

    change to github token

--- bin/cdk-codepipeline-lambda-apigw.ts
+++ bin/cdk-codepipeline-lambda-apigw.ts
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkCodepipelineLambdaApigwStack } from '../lib/cdk-codepipeline-lambda-apigw-stack';
-import { MyPipelineStack } from '../lib/codepipeline-stack'
+import { CdkpipelinesStack } from '../lib/codepipeline-stack'
 
 const app = new cdk.App();
 // new CdkCodepipelineLambdaApigwStack(app, 'CdkCodepipelineLambdaApigwStack', {
 //      env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 // });
-new MyPipelineStack(app, 'myPipelineStack', {
+new CdkpipelinesStack(app, 'myPipelineStack', {
      env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 })
 
